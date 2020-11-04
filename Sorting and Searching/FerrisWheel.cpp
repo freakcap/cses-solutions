4638b52550eae18125bacdf960f5b45d50d66a2e
freakcap <kulkarni.pranav123@gmail.com>
2020-11-04 14:34:58 +0530

    4th nov

---
+++ Sorting and Searching/FerrisWheel.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#define ll          long long int
+#define pb          push_back
+#define vll         vector<ll>
+#define mll         unordered_map<ll,ll>
+#define minpq       priority_queue<ll,vector<ll>,greater<ll>>
+#define maxpq       priority_queue<ll>
+#define endl        "\n"
+#define modAdd(a,b) ((a%M)+(b%M))%M
+#define modMul(a,b) ((a%M)*(b%M))%M
+#define debug(x)    cout<< #x <<"="<<x<<"\n"
+#define fo(i,a,x)   for(ll i=a;i<x;++i)
+#define fastio      ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+ll gcd(ll a, ll b) { return a % b == 0 ? b : gcd(b, a % b); }
+void onlineJ() {freopen("input.txt", "r", stdin); freopen("error.txt", "w", stderr); freopen("output.txt", "w", stdout);}
+
+ll n, m, k;
+const ll M = (ll)1e9 + 7;
+
+void solve()
+{
+    ll n, x; cin >> n >> x;
+    vector<ll>arr(n);
+    for (auto& v : arr) cin >> v;
+    sort(arr.begin(), arr.end());
+    int i = 0, j = arr.size() - 1;
+    ll ans = 0;
+    while (i <= j)
+    {
+        if (arr[j] > x)
+        {
+            j--;
+        }
+        else if (arr[j] + arr[i] > x)
+        {
+            ans++;
+            j--;
+        }
+        else {
+            i++;
+            j--;
+            ans++;
+        }
+    }
+    cout << ans;
+}
+
+int main()
+{
+    fastio;
+    // onlineJ();
+    ll t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
